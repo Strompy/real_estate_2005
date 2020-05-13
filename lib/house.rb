372e9e7b12bbdc5dcaab8befd6a917ab8b9e7f50
Phillip Strom <phillip.strom@gmail.com>
2020-05-13 09:35:40 -0600

    Create above_market_average method and value attribute

--- lib/house.rb
+++ lib/house.rb
@@ -1,16 +1,21 @@
 require './lib/room'
 
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :value
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @value = @price.delete("$").to_i
   end
 
   def add_room(room)
-    @rooms << room 
+    @rooms << room
+  end
+
+  def above_market_average?
+    @value > 500000
   end
 
 end
